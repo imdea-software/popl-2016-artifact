d990c1ca3ea1e64931a17bec0bd20e25a0a8f91d
Michael Emmi <michael.emmi@gmail.com>
2014-11-03 02:00:07 +0100

    Functional-style history updates.

--- lib/History.rb
+++ lib/History.rb
@@ -8,13 +8,32 @@ class History
     @after = {}
   end
 
+  def initialize_copy(other)
+    super
+    @completed = @completed.clone
+    @pending = @pending.clone
+    @before = @before.clone
+    @after = @after.clone
+  end
+
   def each(&block)
-    @completed.each(block)
-    @pending.each(block)
+    if block_given?
+      @completed.each(&block)
+      @pending.each(&block)
+      self
+    else
+      to_enum
+    end
   end
 
   def before(o) @before[o] end
   def after(o) @after[o] end
+  def minimals; select {|o| before(o).empty? } end
+  def maximals; select {|o| after(o).empty? } end
+
+  def start(o) clone.start!(o) end
+  def finish(o) clone.finish!(o) end
+  def remove(o) clone.remove!(o) end
 
   def start!(o)
     @completed.each {|c| @after[c] << o}
@@ -22,13 +41,13 @@ class History
     @before[o] = []
     @before[o].push *@completed
     @after[o] = []
-    o
+    self
   end
 
   def finish!(o)
     @completed << o
     @pending.delete o
-    o
+    self
   end
 
   def remove!(o)
@@ -38,6 +57,6 @@ class History
     @before.each {|_,v| v.delete o}
     @after.delete o
     @after.each {|_,v| v.delete o}
-    o
+    self
   end
 end
