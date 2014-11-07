dccf979e97bee98c67d0d6078036876ccec66fcb
Michael Emmi <michael.emmi@gmail.com>
2014-11-07 18:53:19 +0100

    Improved axioms.

--- lib/theories.rb
+++ lib/theories.rb
@@ -69,6 +69,9 @@ module BasicTheories
     t.yield "(forall ((x id) (y id)) (= (match x y) (and (= (meth x) add) (= (meth y) rm) (= (arg x 0) (ret y 0)))))"
 
     # unmatched
+    t.yield "(forall ((x id)) (= (unmatched x) (and (= (meth x) add) (not (exists ((y id)) (and (= (meth y) rm) (= (ret y 0) (arg x 0))))))))"
+
+    # same, using added/removed instead of nested quantifier
     t.yield "(forall ((x id) (v value)) (=> (and (= (meth x) add) (= (arg x 0) v)) (added v)))"
     t.yield "(forall ((x id) (v value)) (=> (and (= (meth x) rm) (= (ret x 0) v)) (removed v)))"
     t.yield "(forall ((x id)) (= (unmatched x) (and (= (meth x) add) (not (removed (arg x 0))))))"
@@ -76,16 +79,18 @@ module BasicTheories
     # all popped elements are pushed
     t.yield "(forall ((x id)) (=> (and (= (meth x) rm) (not (= (ret x 0) vempty))) (exists ((y id)) (and (= (meth y) add) (= (arg y 0) (ret x 0))))))"
 
-    # same, without nested quantifier
+    # same, using added/removed instead of nested quantifier
     t.yield "(forall ((v value)) (=> (and (not (= v vempty)) (removed v)) (added v)))"
-    t.yield "(forall ((x id) (y id)) (=> (and (not (= x y)) (= (meth x) rm) (= (meth y) rm)) (not (= (ret x 0) (ret y 0)))))"
 
-    # adds before matched rmoves
+    # no two non-empty removes return the same value
+    t.yield "(forall ((x id) (y id)) (=> (and (= (meth x) rm) (= (meth y) rm) (not (= x y)) (not (= (ret x 0) vempty))) (not (= (ret x 0) (ret y 0)))))"
+
+    # adds before matched removes
     t.yield "(forall ((x id) (y id)) (=> (match x y) (lb x y)))"
 
-    # all adds removed before empty rmoves
+    # all adds removed before empty removes
     t.yield "(forall ((x id) (y id) (z id)) (=> (and (match x y) (= (meth z) rm) (= (ret z 0) vempty) (lb x z)) (lb y z)))"
-    t.yield "(forall ((x id) (z id)) (=> (and (unmatched x) (= (meth z) rm) (= (ret z 0) vempty)) (lb x z)))"
+    t.yield "(forall ((x id) (z id)) (=> (and (unmatched x) (= (meth z) rm) (= (ret z 0) vempty)) (lb z x)))"
   end
 
   theory :stack_theory do |t|
