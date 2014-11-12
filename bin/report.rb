57137acfa80b95fe14e931c382e735ce23bf5e6a
Michael Emmi <michael.emmi@gmail.com>
2014-11-12 02:32:52 +0100

    A new report.

--- bin/report.rb
+++ bin/report.rb
@@ -3,14 +3,16 @@
 SOURCES = []
 FLAGSS = []
 COLUMNS = { example: 1, flags: 1, step: 4, viol: 4, time: 10 }
-TIMEOUT = 5
+TIMEOUT = 100
 
 # SOURCES << "examples/generated/bkq-very-concurrent/*.log"
 # SOURCES << "examples/generated/msq-very-concurrent/*.log"
-SOURCES << "examples/generated/bkq-almost-sequential/*.log"
+# SOURCES << "examples/generated/bkq-almost-sequential/*.log"
 # SOURCES << "examples/generated/msq-almost-sequential/*.log"
-SOURCES << "examples/generated/*.log"
-SOURCES << "examples/simple/.log*"
+# SOURCES << "examples/generated/*.log"
+# SOURCES << "examples/simple/.log*"
+
+SOURCES << "examples/generated/my-sync-stack.*.log"
 
 FLAGSS << "-a saturation"
 FLAGSS << "-a saturation -r"
