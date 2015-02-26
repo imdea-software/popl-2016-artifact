61329d07cd3e2eead1c53b14ef7ef7d5606b5b32
Michael Emmi <michael.emmi@gmail.com>
2015-02-26 17:01:30 +0100

    Idea for parameterized matching.

--- lib/matching.rb
+++ lib/matching.rb
@@ -4,7 +4,19 @@ module Matching
   ADD_PATTERN = /add|put|push|enqueue/
   REM_PATTERN = /rem|get|pop|dequeue/
 
+  # TODO this predicate will be an input…
+  def self.collection_match?(history,x,y)
+    history.method_name(x) =~ ADD_PATTERN &&
+    history.method_name(y) =~ REM_PATTERN &&
+    history.arguments(x) == history.returns(y) ||
+    history.method_name(x) =~ ADD_PATTERN && x == y
+  end
+
   def self.get(history,id)
+
+    # TODO replace what follows with this one generic line…
+    # history.find {|m| self.collection_match?(history,m,id)}
+
     case history.method_name(id)
     when ADD_PATTERN
       :none
@@ -22,5 +34,6 @@ module Matching
     else
       nil
     end
+
   end
 end
