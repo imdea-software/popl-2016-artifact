7815168b9f6d2bba762e4db5285b461c26a9d9e2
Michael Emmi <michael.emmi@gmail.com>
2015-03-03 01:23:42 +0100

    Cleaner matching.

--- lib/matching.rb
+++ lib/matching.rb
@@ -6,34 +6,16 @@ module Matching
 
   # TODO this predicate will be an input…
   def self.collection_match?(history,x,y)
+    history.method_name(x) =~ ADD_PATTERN && x == y ||
+    history.method_name(x) =~ REM_PATTERN && x == y && history.returns(y) == [:empty] ||
     history.method_name(x) =~ ADD_PATTERN &&
     history.method_name(y) =~ REM_PATTERN &&
-    history.arguments(x) == history.returns(y) ||
-    history.method_name(x) =~ ADD_PATTERN && x == y
+    history.arguments(x) == history.returns(y)
   end
 
   def self.get(history,id)
 
-    # TODO replace what follows with this one generic line…
-    # history.find {|m| self.collection_match?(history,m,id)}
-
-    case history.method_name(id)
-    when ADD_PATTERN
-      id
-    when REM_PATTERN
-      if history.returns(id).nil?
-        nil
-      elsif history.returns(id).first == :empty
-        id
-      else
-        history.find do |a|
-          history.method_name(a) =~ ADD_PATTERN &&
-          history.arguments(a) == history.returns(id)
-        end
-      end
-    else
-      nil
-    end
+    return history.find {|m| self.collection_match?(history,m,id)}
 
   end
 end
